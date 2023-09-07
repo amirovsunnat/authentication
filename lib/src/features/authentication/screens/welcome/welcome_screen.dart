a9d48ec7006d1f49b36480c347501f9093335cfd
Sunnat <sunnatbekamirov22@gmail.com>
2023-09-07 23:12:24 +0500

    Splash screen change.

--- lib/src/features/authentication/screens/welcome/welcome_screen.dart
+++ lib/src/features/authentication/screens/welcome/welcome_screen.dart
@@ -5,6 +5,8 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: Text("Welcome!"),
+    );
   }
 }
